3df85788b687f0f6ef19c809f1b335c31c42005c
Frederik Ring <frederik.ring@gmail.com>
2022-01-06 11:25:42 +0100

    Add documentation for JS client

--- client/README.md
+++ client/README.md
@@ -1,2 +1,74 @@
 # client
 Reference client implementing the Offen protocol
+
+## Usage
+
+The library is currently not available on `npm` or any other package registry.
+All code is contained in `index.js` in ESM format.
+We might consider publishing it as a npm package once the protocol has become stable.
+
+### Constructing a new client
+
+The client constructor expects an absolute URL for the endpoint.
+Further options can be passed in a second and optional argument.
+
+```js
+const client = new OffenProtocolClient('https://example.com/protocol-endpoint', options)
+```
+
+The following options can be passed to a client:
+
+#### `fetch`
+
+A [`fetch`][mdn-fetch] compatible function that is used for calling the endpoint.
+Defaults to `window.fetch`.
+Pass a wrapped version of `fetch` if you need to customize behavior.
+
+[mdn-fetch]: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
+
+#### `serializeBody`
+
+A function that is used to serialize the request body for `register`, `submit` and `purge` calls.
+Defaults to `JSON.stringify`.
+Pass a custom serializer that accepts a single argument and returns a string your server expects non-JSON encodings.
+The function can be either synchronous or asynchronous.
+
+#### `contentType`
+
+The value sent in the `Content-Type` headers when a request body is sent.
+Defaults to `application/json`.
+Pass a different value in case your server expects other types of payloads, also see `serializeBody`.
+
+#### `handleResponse`
+
+An function that is handling the fetch response.
+By default it expects a 2xx response and a JSON response that is parsed and returned as a Javascript object.
+204 responses are expected to be empty and return `null`.
+The function can be either synchronous or asynchronous.
+
+### Methods on the client
+
+The client exposes a method for each action defined in the protocol.
+All methods return a Promise.
+
+#### `probe({ params })`
+
+Send a __probe__ request to the endpoint, appending the given `params` to the URL.
+This request intentionally omits sending cookies.
+
+#### `register({ params, body })`
+
+Send a __register__ request to the endpoint, appending the given `params` to the URL and sending the given `body`.
+This request intentionally omits sending cookies.
+
+#### `submit({ params, body })`
+
+Send a __submit__ request to the endpoint, appending the given `params` to the URL and sending the given `body`.
+
+#### `query({ params })`
+
+Send a __query__ request to the endpoint, appending the given `params` to the URL.
+
+#### `purge({ params, body })`
+
+Send a __purge__ request to the endpoint, appending the given `params` to the URL and sending the given `body`.
--- client/index.js
+++ client/index.js
@@ -1,39 +1,21 @@
-export default class Client {
+export default class OffenProtocolClient {
   constructor (endpoint, options = {}) {
     this.endpoint = endpoint
     this.fetch = options.fetch || window.fetch.bind(window)
     this.serializeBody = options.serializeBody || JSON.stringify
     this.contentType = options.contentType || 'application/json'
-    this.handleResponse = options.handleResponse || handleResponse
-  }
-
-  _request (method, credentials, { body, params }) {
-    const url = new window.URL(this.endpoint)
-    if (params) {
-      url.search = new window.URLSearchParams(params)
-    }
-    return this.fetch(url, {
-      method,
-      credentials,
-      body: typeof body === 'undefined'
-        ? undefined
-        : this.serializeBody(body),
-      headers: {
-        'Content-Type': this.contentType
-      }
-    })
-      .then(this.handleResponse)
+    this.handleResponse = options.handleResponse || this._handleResponse
   }
 
   probe ({ params } = {}) {
     return this._request('GET', 'omit', { params })
   }
 
-  register ({ body, params } = {}) {
+  register ({ params, body } = {}) {
     return this._request('POST', 'omit', { body, params })
   }
 
-  submit ({ body, params } = {}) {
+  submit ({ params, body } = {}) {
     return this._request('PUT', 'include', { body, params })
   }
 
@@ -41,28 +23,52 @@ export default class Client {
     return this._request('GET', 'include', { params })
   }
 
-  purge ({ body, params } = {}) {
+  purge ({ params, body } = {}) {
     return this._request('DELETE', 'include', { body, params })
   }
-}
 
-function handleResponse (response) {
-  if (response.status >= 400) {
-    return response.clone().json()
-      .catch(function () {
-        return response.text()
-          .then(function (rawBody) {
-            return { error: rawBody }
-          })
+  _request (method, credentials, { body, params }) {
+    const url = new window.URL(this.endpoint)
+    if (params) {
+      url.search = new window.URLSearchParams(params)
+    }
+
+    const serializedBody = typeof body === 'undefined'
+      ? undefined
+      : this.serializeBody(body)
+
+    return Promise.resolve(serializedBody)
+      .then((body) => {
+        return this.fetch(url, {
+          method,
+          credentials,
+          body,
+          headers: body
+            ? { 'Content-Type': this.contentType }
+            : undefined
+        })
       })
-      .then(function (errorBody) {
-        const err = new Error(errorBody.error)
-        err.status = response.status
-        throw err
-      })
+      .then(this.handleResponse)
   }
-  if (response.status !== 204) {
-    return response.json()
+
+  _handleResponse (response) {
+    if (response.status >= 400) {
+      return response.clone().json()
+        .catch(function () {
+          return response.text()
+            .then(function (rawBody) {
+              return { error: rawBody }
+            })
+        })
+        .then(function (errorBody) {
+          const err = new Error(errorBody.error)
+          err.status = response.status
+          throw err
+        })
+    }
+    if (response.status !== 204) {
+      return response.json()
+    }
+    return null
   }
-  return null
 }
